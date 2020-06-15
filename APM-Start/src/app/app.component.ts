bf9e6de8d5ebbf7fdb59e6f4d0060b4d292280fe
dennoken <mickael.dagrassa@gmail.com>
2020-06-15 21:09:24 +0200

    add comps

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <nav class="navbar navbar-expand navbar-light bg-light">
+      <ul class="nav navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="/"
+            >{{ pageTitle }} <span class="sr-only">(current)</span></a
+          >
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" [routerLink]="['/products']">Products List</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management'
 }
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,15 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule, Routes } from '@angular/router'
+import { AppComponent } from './app.component'
+import { WelcomeComponent } from './home/welcome.component'
+import { ProductDetailComponent } from './products/product-detail.component'
+import { ProductListComponent } from './products/product-list.component'
+import { ConverToSpacesPipe } from './shared/convert-to-spaces.pipe'
+import { StarComponent } from './shared/star.component'
 
-import { AppComponent } from './app.component';
+export const routes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+  { path: '**', pathMatch: 'full', redirectTo: 'welcome' },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ConverToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
